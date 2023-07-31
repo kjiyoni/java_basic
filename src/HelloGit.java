a476b6e94a1945ecac1e5ea74b993fa1a3fc57db
kjiyoni <kjiyoni17@gmail.com>
2023-07-31 16:40:04 +0900

    homework

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 		System.out.println("111111111111111");
 		System.out.println("222222222222222");
 		System.out.println("3333333333333");
+		System.out.println("집에서 작업을 하였습니다...");
 	}
 }
